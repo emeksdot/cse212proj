741ebd7b5bd0986c1143de2067e2eb7fdebd1b29
Emeksdot <emeksdot@yahoo.com>
2026-03-10 14:55:51 +0100

    completed the code tasks and ran the tests successfully

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,21 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        // ___________________________________________________________________________
 
-        return []; // replace this return statement with your own
+        // 1. First I create an Array of double that I call results of size - length
+        // 2. I use a for loop to populate the array using the relation: 
+        // results[i] = number * (double)(i + 1);
+        // 3. I return the array results
+
+        double[] results = new double[length];
+        // results[0] = number;
+        for (int i = 0; i < length; i++)
+        {
+            results[i] = number * (double)(i + 1);
+        }
+
+        return results; // replace this return statement with your own
     }
 
     /// <summary>
@@ -25,9 +38,20 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // 1. I start by subtracting the given amount from the size of the data list to get the index of the element that would be the first element of the results array, I store this value in a variable called indexValue. 
+
+        int indexValue = data.Count - amount;
+
+        // 2. Using a for loop I add more elements, (the number of elements I add is equal to indexValue) to the end of the results List
+
+        for (int i = 0; i < indexValue; i++)
+        {
+            data.Add(data[i]);
+        }
+
+        // 3. I finally use the RemoveRange method to remove exactly indexValue number of elements from the start of the data List.
+
+        data.RemoveRange(0, indexValue);
+
     }
 }
