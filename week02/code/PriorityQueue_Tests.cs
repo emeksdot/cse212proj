b938f46873e1f12ef526cabb2c08361a7a575137
Emeksdot <emeksdot@yahoo.com>
2026-03-19 16:04:40 +0100

    completed the test cases, ran them and they all passed

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -11,8 +11,20 @@ public class PriorityQueueTests
     // Defect(s) Found: 
     public void TestPriorityQueue_1()
     {
+        // Arrange
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("Watch", 167);
+        priorityQueue.Enqueue("Shoes", 150);
+        priorityQueue.Enqueue("Bag", 120);
+        priorityQueue.Enqueue("Hat", 160);
+        // var item5 = new PriorityItem("Hat", 160);
+
+
+        // Act
+        string result = priorityQueue.Dequeue();
+
+        // Assert
+        Assert.AreEqual("Watch", result);
     }
 
     [TestMethod]
@@ -21,8 +33,20 @@ public class PriorityQueueTests
     // Defect(s) Found: 
     public void TestPriorityQueue_2()
     {
+        // Arrange
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("Watch", 167);
+        priorityQueue.Enqueue("Shoes", 150);
+        priorityQueue.Enqueue("Bag", 120);
+        priorityQueue.Enqueue("Hat", 160);
+        priorityQueue.Enqueue("Suit", 167);
+        // Assert.Fail("Implement the test case and then remove this.");
+
+        // Act
+        string result = priorityQueue.Dequeue();
+
+        // Assert
+        Assert.AreEqual("Watch", result);
     }
 
     // Add more test cases as needed below.
