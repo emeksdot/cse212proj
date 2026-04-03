c5c5c8eec440dcaaab42f89f2fc0ef58fc3baeb5
Emeksdot <emeksdot@yahoo.com>
2026-04-03 20:14:02 +0100

    SumSquaresRecursive function completed and tested

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -15,7 +15,17 @@ public static class Recursion
     public static int SumSquaresRecursive(int n)
     {
         // TODO Start Problem 1
-        return 0;
+        // Base Case
+        if (n <= 0)
+        {
+            return 0;
+        }
+        else
+        {
+            // Recursive Case
+            return n * n + SumSquaresRecursive(n - 1);
+        }
+        // return 0;
     }
 
     /// <summary>
@@ -129,10 +139,11 @@ public static class Recursion
     {
         // If this is the first time running the function, then we need
         // to initialize the currPath list.
-        if (currPath == null) {
+        if (currPath == null)
+        {
             currPath = new List<ValueTuple<int, int>>();
         }
-        
+
         // currPath.Add((1,2)); // Use this syntax to add to the current path
 
         // TODO Start Problem 5
